8609af761c237757ab44bfb35788456475ec5af3
Will Harris <will.t.harris@protonmail.com>
2020-04-06 15:28:54 -0700

    rename components

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-layout`,
       options: {
-        component: require.resolve("./src/components/layout.js"),
+        component: require.resolve("./src/components/Layout.js"),
       },
     },
     {
