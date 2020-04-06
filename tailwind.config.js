7c6d552a64e50818872bad4de830131c792a6e9e
Will Harris <will.t.harris@protonmail.com>
2020-04-06 16:24:08 -0700

    add font to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   theme: {
-    extend: {},
+    fontFamily: {
+      body: ["Montserrat", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        navbarBackground: "#2B2728",
+      },
+    },
   },
   variants: {},
   plugins: [],
