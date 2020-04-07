2db7ea21f6549d625ccde20ab70c11b6fcf9296f
Will Harris <will.t.harris@protonmail.com>
2020-04-07 12:48:03 -0700

    change class to className

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,22 +8,22 @@ const toggleMenu = event => {
 
 const Header = ({ siteTitle }) => (
   <header>
-    <nav class="flex items-center justify-between flex-wrap bg-navbarBackground p-6 fixed w-full z-10 top-0 navbar-linear-gradient">
-      <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <nav className="flex items-center justify-between flex-wrap bg-navbarBackground p-6 fixed w-full z-10 top-0 navbar-linear-gradient">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
         <a
-          class="text-white no-underline hover:text-white hover:no-underline"
+          className="text-white no-underline hover:text-white hover:no-underline"
           href="#"
         >
-          <span class="font-body font-bold font-semibold text-2xl pl-2">
+          <span className="font-body font-bold font-semibold text-2xl pl-2">
             willharris.dev
           </span>
         </a>
       </div>
 
-      <div class="block lg:hidden">
+      <div className="block lg:hidden">
         <button
           id="nav-toggle"
-          class="flex items-center px-3 py-2 text-gray-500 border-gray-600 hover:text-white hover:border-white"
+          className="flex items-center px-3 py-2 text-gray-500 border-gray-600 hover:text-white hover:border-white"
           onClick={toggleMenu}
         >
           <svg
@@ -41,34 +41,37 @@ const Header = ({ siteTitle }) => (
       </div>
 
       <div
-        class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block pt-6 lg:pt-0"
+        className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block pt-6 lg:pt-0"
         id="nav-content"
       >
-        <ul class="list-reset lg:flex justify-end flex-1 items-center">
-          <li class="mr-3">
-            <a class="inline-block py-2 px-4 text-white no-underline" href="#">
+        <ul className="list-reset lg:flex justify-end flex-1 items-center">
+          <li className="mr-3">
+            <a
+              className="inline-block py-2 px-4 text-white no-underline"
+              href="#"
+            >
               Active
             </a>
           </li>
-          <li class="mr-3">
+          <li className="mr-3">
             <a
-              class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
+              className="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
               href="#"
             >
               link
             </a>
           </li>
-          <li class="mr-3">
+          <li className="mr-3">
             <a
-              class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
+              className="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
               href="#"
             >
               link
             </a>
           </li>
-          <li class="mr-3">
+          <li className="mr-3">
             <a
-              class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
+              className="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
               href="#"
             >
               link
