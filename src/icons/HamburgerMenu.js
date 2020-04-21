f987bd5a4d54e7b02676d37f3cdcf3278ba9472d
Will Harris <will.t.harris@protonmail.com>
2020-04-21 12:18:03 -0700

    add title to hamburger svg

--- src/icons/HamburgerMenu.js
+++ src/icons/HamburgerMenu.js
@@ -3,6 +3,7 @@ import React from "react"
 export default function HamburgerMenu() {
   return (
     <svg width="22" height="19" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <title>Click to view menu items</title>
       <path
         d="M.786 3.375h20.428c.434 0 .786-.336.786-.75V.75c0-.414-.352-.75-.786-.75H.786C.352 0 0 .336 0 .75v1.875c0 .414.352.75.786.75zm0 7.5h20.428c.434 0 .786-.336.786-.75V8.25c0-.414-.352-.75-.786-.75H.786C.352 7.5 0 7.836 0 8.25v1.875c0 .414.352.75.786.75zm0 7.5h20.428c.434 0 .786-.336.786-.75V15.75c0-.414-.352-.75-.786-.75H.786c-.434 0-.786.336-.786.75v1.875c0 .414.352.75.786.75z"
         fill="#fff"
