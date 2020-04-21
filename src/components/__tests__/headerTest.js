bb16e4563f53a530629f692d194145e97bae85d0
Will Harris <will.t.harris@protonmail.com>
2020-04-21 12:19:35 -0700

    test to confirm hamburger not visible on desktop

--- src/components/__tests__/headerTest.js
+++ src/components/__tests__/headerTest.js
@@ -3,14 +3,20 @@ import { render } from "@testing-library/react"
 
 import Header from "../Header"
 
-test("Matches previous snapshot", () => {
+test("matches previous snapshot", () => {
   const header = render(<Header />)
 
   expect(header).toMatchSnapshot()
 })
 
-test("Displays the correct title", () => {
+test("displays the correct title", () => {
   const { getByText } = render(<Header />)
 
   expect(getByText("willharris.dev")).toBeTruthy()
+})
+
+test("hamburger icon not visible on desktop view", () => {
+  const { getByTitle } = render(<Header />)
+
+  expect(getByTitle("Click to view menu items")).not.toBeVisible()
 })
