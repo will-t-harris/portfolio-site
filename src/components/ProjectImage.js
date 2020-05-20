c8a8ffc4a70a196922672f3f5a9dd588ad4e6ae9
Will Harris <will.t.harris@protonmail.com>
2020-05-20 15:08:49 -0700

    refactor: make component only handle image display

--- src/components/ProjectImage.js
+++ src/components/ProjectImage.js
@@ -3,25 +3,18 @@ import PropTypes from "prop-types"
 
 import Img from "gatsby-image"
 
-const ProjectImage = ({ data, url, border }) => (
-  <div className="flex pb-16 max-w-4xl">
+const ProjectImage = ({ data, border }) => (
+  <div className="flex flex-col mt-10">
     <Img
-      fixed={data.childImageSharp.fixed}
-      className={`border-2 ${border} mx-auto lg:ml-64`}
+      fixed={data.fixed}
+      className={`self-center border-2 ${border} mx-auto lg:ml-64`}
     />
-    <a
-      href={`https://www.${url}`}
-      className={`font-body font-semibold self-center mr-12 lg:mr-auto border-b-2 ${border}`}
-    >
-      {url}
-    </a>
   </div>
 )
 
 export default ProjectImage
 
 ProjectImage.propTypes = {
-  url: PropTypes.string.isRequired,
   border: PropTypes.string.isRequired,
   data: PropTypes.object.isRequired,
 }
