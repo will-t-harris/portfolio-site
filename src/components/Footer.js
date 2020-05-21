2fc426b3e36542a1f3004c5dc82e7088ab62f7ef
Will Harris <will.t.harris@protonmail.com>
2020-05-20 22:12:53 -0700

    style: pass icon colors in via props

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,20 +11,23 @@ const Footer = ({ resume }) => {
     <div className="bg-black py-8">
       <div className="flex justify-evenly font-body font-semibold text-white">
         <div className="flex">
-          <Link to="#" className="pr-4 border-r-2 border-primary">
-            Contact
+          <Link
+            to="#"
+            className="pr-4 border-r-2 border-primary hover:text-accent"
+          >
+            CONTACT
           </Link>
-          <a href={resume} className="pl-4" download>
-            Resume
+          <a href={resume} className="pl-4 hover:text-accent" download>
+            RESUME
           </a>
         </div>
         <div className="lg:flex hidden">
           <CodeIcon /> 2020 Will Harris
         </div>
         <div className="flex">
-          <GithubIcon twClasses="mr-2 icon-transition" />
-          <TwitterIcon twClasses="mr-2 icon-transition" />
-          <LinkedInIcon twClasses=" icon-transition" />
+          <GithubIcon twClasses="mr-2 hover:text-accent" />
+          <TwitterIcon twClasses="mr-2 hover:text-accent" />
+          <LinkedInIcon twClasses="hover:text-accent" />
         </div>
       </div>
     </div>
