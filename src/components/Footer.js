e0270e744412524129762cdc0e9bbee6644da468
Will Harris <will.t.harris@protonmail.com>
2020-05-23 13:32:03 -0700

    refactor: simplify footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,20 +11,6 @@ const Footer = ({ resume }) => {
     <div className="bg-black py-8">
       <div className="flex justify-evenly font-body font-semibold text-white">
         <div className="flex">
-          <Link
-            to="#"
-            className="pr-4 border-r-2 border-primary hover:text-accent"
-          >
-            CONTACT
-          </Link>
-          <a href={resume} className="pl-4 hover:text-accent" download>
-            RESUME
-          </a>
-        </div>
-        <div className="lg:flex hidden">
-          <CodeIcon /> 2020 Will Harris
-        </div>
-        <div className="flex">
           <GithubIcon twClasses="mr-2 hover:text-accent" />
           <TwitterIcon twClasses="mr-2 hover:text-accent" />
           <LinkedInIcon twClasses="hover:text-accent" />
